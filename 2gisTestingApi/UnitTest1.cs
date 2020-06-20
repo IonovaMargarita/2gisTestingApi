d596419022d35e661b9ea1c2780a07e5eddc30d3
Маргарита <ionovam1990@gmail.com>
2020-06-20 18:23:24 +0400

    Добавьте файлы проекта.

---
+++ 2gisTestingApi/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace _2gisTestingApi
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
